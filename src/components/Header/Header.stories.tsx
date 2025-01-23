b790b0bfa42de93bce9ff64f5c508f20df0ca9f7
Nathan Harris <nharrisanalyst@gmail.com>
2025-01-23 12:36:33 -0800

    first commit

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { withRouter } from 'storybook-addon-react-router-v6';
+
+
+import Header from './';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Header',
+  component: Header,
+  decorators: [
+// DECORATOR #1
+  (Story) => (
+    <div style={{ width:'1000px' }}>
+      <Story />
+    </div>
+  ),
+  withRouter],
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args:{},
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
+
+
+export const Navigated: Story = {
+  args: {
+    navigation:'/',
+  },
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import './header.css';
+
+type HeaderProps = {
+    navigation:string;
+}
+
+const Header = ({navigation = ""}:HeaderProps)=>{
+    const homeClass = navigation === "/" ? "activeNow" : "";
+    const workClass = navigation.match(/^\/work/) ? "activeNow" : "";
+    const contactClass = navigation.match(/^\/contact/) ? "activeNow" : "";
+
+    return(
+            <div className='main-header'>
+                <span className='main-name'>Nathan <span className='name-last'>Harris</span></span>
+                <span className='main-links'>
+                <Link className={homeClass} to='/'>Home</Link>
+                <Link className={workClass} to='/work'>Work</Link>
+                <Link to="https://docs.google.com/document/d/1BE_2vCPxvHcG45s7CJixCnfJkG5uFMPkpvjyfU24xo0/edit?usp=sharing">Resume</Link>
+                <Link className={contactClass} to='/Contact'>Contact</Link>
+
+                </span>
+            </div>
+    )
+    }
+
+export default Header;
