a816477734501a172e606901cd7a554ebea083d9
Nathan Harris <nharrisanalyst@gmail.com>
2025-01-30 13:46:35 -0800

    added card and animation

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { withRouter } from 'storybook-addon-react-router-v6';
+
+
+import Card from './';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Card',
+  component: Card,
+  decorators: [(Story) => (
+    <div style={{ width:'1008x' }}>
+      <Story />
+    </div>
+  ),
+  withRouter],
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args:{},
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    imageSrc:"/images/work/JPMC_Savings/2.png",
+    linkTo:'/work/JPMC_Savings',
+    name:"JPMC_Savings",
+  },
+};
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,45 @@
+import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import './card.css';
+
 
 export type CardProps ={
     imageSrc:string;
     linkTo:string;
+    name:string;
 }
 
-const Card = ({imageSrc,linkTo}:CardProps) =>{
+const Card = ({imageSrc,linkTo, name}:CardProps) =>{
+    const [showBanner, setShowBanner] = useState<boolean>(false);
+    const [opacity, setOpacity] = useState<number>(0);
     const link = `/work/${linkTo}`;
+    const handleOnHover=()=>{
+        setShowBanner(true);
+    }
+    const handHoverOut =()=>{
+        setShowBanner(false);
+    }
+    
+
+    useEffect(()=>{
+        console.log('changing')
+        let newOpacity = showBanner?0.5:0;
+        setOpacity(newOpacity);
+
+    },[showBanner])
+    let classBanner = opacity ===0?"card-banner":"card-banner banner-appear";
     return(
-        <div className='card-container' >
+        <div className='card-container' 
+             onMouseEnter={() => handleOnHover()}
+             onMouseLeave={() => handHoverOut()}    
+        >
             <Link to={link}>
+            <AspectRatio ratio={24/12}>
                 <img src={imageSrc} />
-            </Link>
+            </AspectRatio>
+            </Link> 
+            <div className={classBanner}>{name.replace("_"," ")}</div>
         </div>
     )
 }
