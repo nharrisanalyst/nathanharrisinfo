75eda6b7c641a1cf8a15edaf5413f788a92ca434
Nathan Harris <nharrisanalyst@gmail.com>
2025-02-05 11:53:22 -0800

    updates'

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -5,6 +5,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Footer from './';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+
+// @ts-ignore
 const meta = {
   title: 'Footer',
   component: Footer,
